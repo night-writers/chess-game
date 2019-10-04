f845d20e38917c10534f1f3954e53c35639c4419
Daniel Ayala <dayala+github@protonmail.com>
2019-10-04 03:26:51 +0000

    Made some association adjustments based on conversation on Slack channel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :games
-  has_many :pieces
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
