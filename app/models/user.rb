de67f64fac73c8a30d94a1df6b62568ac38d1466
David Bonnaud <david@davidbonnaud.com>
2019-10-06 11:46:50 +0000

    added activerecord-session_store gem to exceed cookie overflow cap due to twitter integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook twitter gplus]
   
+  def email_required?
+    false
+  end
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
