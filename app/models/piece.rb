95c80c3383e1987cba1b97e439d4b79203bc07ec
Will Daskam <willd2122@gmail.com>
2019-10-11 20:30:26 +0000

    Added is_obstructed? method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,4 +3,108 @@ module Piece
   def self.table_name_prefix
     'piece_'
   end
+  
+  def self.is_obstructed_horizontal?(xd, xl, yl)
+    if (xl > xd) && (xl - 1) != xd
+      b = xl
+        while (b > xd) && (b - 1) != xd 
+          b = b - 1
+          if self.is_occupied(b, yl)
+            return true
+          end
+        end
+    end
+    if (xl < xd) && (xl + 1) != xd
+      c = xl
+      while (c < xd) && (c + 1) != xd
+        c = c + 1
+        if self.is_occupied(c, yl) 
+          return true
+        end
+      end
+    end
+      return false
+  end
+  
+  def self.is_obstructed_vertical?(yd, yl, xl)
+    if (yl > yd) && (yl - 1) != yd
+      f = yl
+        while (f > yd) && (f - 1) != yd
+          f = f - 1
+          if self.is_occupied(xl, f) 
+            return true
+          end
+        end
+    end
+    if (yl < yd) && (yl + 1) != yd
+      h = yl
+      while (h < yd) && (h + 1) != yd
+        h = h + 1
+        if self.is_occupied(xl, h) 
+          return true
+        end
+      end
+    end
+      return false
+  end
+  
+  def self.is_obstructed_diagonal?(xd, yd, xl, yl)
+    if (yl > yd) && (yl - 1) != yd
+      j = yl
+        while (j > yd) && (j - 1) != yd
+          j = j - 1
+          k = xl
+          if xl > xd
+            k = k - 1
+          elsif xl < xd
+            k = k + 1
+          end
+          if self.is_occupied(k, j) 
+            return true
+          end
+        end
+    end
+    if (yl < yd) && (yl + 1) != yd
+      j = yl
+        while (j < yd) && (j + 1) != yd
+          j = j + 1
+          k = xl
+          if xl > xd
+            k = k - 1
+          elsif xl < xd
+            k = k + 1
+          end
+          if self.is_occupied(k, j) 
+            return true
+          end
+        end
+    end
+      return false
+  end
+  
+  def self.is_obstructed?(location_x, location_y, destination_x, destination_y)
+    if location_y == destination_y
+      self.is_obstructed_horizontal?(destination_x, location_x, location_y)
+    elsif location_x == destination_x
+      self.is_obstructed_vertical?(destination_y, location_y, location_x)
+    elsif  (destination_x - location_x) != (destination_y - location_y)
+      return "invalid input.  Not diagnal, horizontal, or vertical."
+    else location_x != destination_x && location_y != destination_y
+      self.is_obstructed_diagonal?(destination_x, destination_y, location_x, location_y)
+    end
+    
+  end
+  
+  # Temporary method until functionality for adding pieces to a game is added.
+  def self.is_occupied(x, y)
+    a = 2
+    b = 2
+    if a == 2 && b == 2
+      return true
+    else
+      return false
+    end
+    #game.pieces.where(location_x: x, location_y: y).present?
+  end
+
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
+# Temporary tests until functionality for adding pieces to a game is added.
+  it "should return true if there is a horizontal obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(4, 2, 1, 2)).to eq true
+    end
+    it "should return true if there is a vertical obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(2, 4, 2, 1)).to eq true
+    end
+    it "should return true if there is a diagonal obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(4, 4, 1, 1)).to eq true
+    end
+    it "should return false if there is no horizontal obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(2, 2, 1, 2)).to eq false
+    end
+    it "should return false if there is no vertical obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(2, 2, 2, 1)).to eq false
+    end
+    it "should return false if there is no diagonal obstruction" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(2, 2, 1, 1)).to eq false
+    end
+    it "should return invalid if the input is invalid" do
+      game = FactoryBot.create(:game)
+      expect(Piece.is_obstructed?(4, 4, 1, 5)).to eq "invalid input.  Not diagnal, horizontal, or vertical."
+    end
 end
