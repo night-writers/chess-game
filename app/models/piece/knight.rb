a3869fb7b794c771b36a5b8cca37cfa3750e7ab0
Daniel Ayala <dayala+github@protonmail.com>
2019-10-22 15:27:05 +0000

    Added logic to validate knight moves. Corresponding tests also added.

--- app/models/piece/knight.rb
+++ app/models/piece/knight.rb
@@ -1,2 +1,22 @@
 class Piece::Knight < Piece
+  def self.valid_move?(origin_x, origin_y, destination_x, destination_y)
+    # If lateral movement is one space, allow two-space movement along y-axis
+    # If lateral movement is two spaces, allow single-space movement along y-axis
+    # No checks for obstruction, as knight can "jump" over pieces
+    if ((destination_x - origin_x) == 1) || ((origin_x - destination_x) == 1)
+      if ((destination_y - origin_y) == 2) || ((origin_y - destination_y) == 2)
+        return true
+      else
+        return false
+      end
+    elsif ((destination_x - origin_x) == 2) || ((origin_x - destination_x) == 2)
+      if ((destination_y - origin_y) == 1) || ((origin_y - destination_y) == 1)
+        return true
+      else
+        return false
+      end
+    else #no valid movement conditions for knight met
+      return false
+    end
+  end
 end
