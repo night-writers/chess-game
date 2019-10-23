673d394f3eb5d0db744d1c679a68de35290792fb
Daniel Ayala <dayala+github@protonmail.com>
2019-10-23 03:54:44 +0000

    Changed origin to location, as suggested by team, for consistency.

--- app/models/piece/knight.rb
+++ app/models/piece/knight.rb
@@ -1,16 +1,15 @@
 class Piece::Knight < Piece
-  def self.valid_move?(origin_x, origin_y, destination_x, destination_y)
+  def self.valid_move?(location_x, location_y, destination_x, destination_y)
     # If lateral movement is one space, allow two-space movement along y-axis
     # If lateral movement is two spaces, allow single-space movement along y-axis
-    # No checks for obstruction, as knight can "jump" over pieces
-    if ((destination_x - origin_x) == 1) || ((origin_x - destination_x) == 1)
-      if ((destination_y - origin_y) == 2) || ((origin_y - destination_y) == 2)
+    if ((destination_x - location_x) == 1) || ((location_x - destination_x) == 1)
+      if ((destination_y - location_y) == 2) || ((location_y - destination_y) == 2)
         return true
       else
         return false
       end
-    elsif ((destination_x - origin_x) == 2) || ((origin_x - destination_x) == 2)
-      if ((destination_y - origin_y) == 1) || ((origin_y - destination_y) == 1)
+    elsif ((destination_x - location_x) == 2) || ((location_x - destination_x) == 2)
+      if ((destination_y - location_y) == 1) || ((location_y - destination_y) == 1)
         return true
       else
         return false
