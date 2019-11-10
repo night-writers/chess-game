4f248b2eae53169590b3049aafbc057b639c0d5f
David Bonnaud <david@davidbonnaud.com>
2019-11-10 02:25:20 +0000

    updated king args and updated white space ajax to update placeholder coordinates instead of actual location as well

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,5 +1,5 @@
 class King < Piece
-  def valid_move?(location_x, location_y, destination_x, destination_y)
+  def valid_move?(location_x, location_y, destination_x, destination_y, game)
     if (location_x - destination_x) <= 1 && (location_y - destination_y) <= 1 && (destination_x - location_x) <= 1 && (destination_y - location_y) <= 1
       return true
     else
